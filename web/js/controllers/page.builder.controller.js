e95bb69a0bb72844f90857ac9ccb33259ec392e9
Mohamed Amine ABBADI <Mohamed.ABBADI@ouicare.net>
2016-12-06 11:43:13 +0100

    Fixe bug duplication contenu layout

--- web/js/controllers/page.builder.controller.js
+++ web/js/controllers/page.builder.controller.js
@@ -3,41 +3,40 @@ function PageBuilderController($scope, toastr) {
     var vm = $scope;
     vm.items = [];
     vm.name = 'The page builder ';
-    vm.headers = [{title: 'Header 1', locked: true, type: 'header'}, {title: 'Header 2', locked: true, type: 'header'}];
-    vm.layouts = [{title: '1 Column', text: "1 x 100%", type: 'layout', composants: []}, {title: '2 Columns', text: "2 x 50%", type: 'layout', left: [], right: []}];
+    vm.components = {
+        headers: [
+            {title: 'Header 1', locked: true, type: 'header'},
+            {title: 'Header 2', locked: true, type: 'header'}
+        ],
+        layouts: [
+            {title: '1 Column', text: "1 x 100%", type: 'layout', composants: []},
+            {title: '2 Columns', text: "2 x 50%", type: 'layout', left: [], right: []}
+        ]
+    };
+
+    vm.orig_components = angular.copy(vm.components);
+
     vm.model = [];
     $scope.sortableOptions = {
         update: function (e, ui) {
-            console.log(ui.item.sortable.model);
-            console.log(ui.item.sortable.droptargetModel[ui.item.sortable.dropindex]);
-            if(!ui.item.sortable.received && ui.item.sortable.droptargetModel[ui.item.sortable.dropindex] && ui.item.sortable.droptargetModel[ui.item.sortable.dropindex].type === "layout" && ui.item.sortable.sourceModel[ui.item.sortable.index].type === "layout"){
+            console.log(vm.items);
+            if (!ui.item.sortable.received && ui.item.sortable.droptargetModel[ui.item.sortable.dropindex] && ui.item.sortable.droptargetModel[ui.item.sortable.dropindex].type === "layout" && ui.item.sortable.sourceModel[ui.item.sortable.index].type === "layout") {
                 toastr.success("Layout within layout are not allowed", 'error');
                 ui.item.sortable.cancel();
             }
-            // if the element is removed from the first container
-            if ($(e.target).hasClass('first') &&
-                    ui.item.sortable.droptarget &&
-                    e.target != ui.item.sortable.droptarget[0]) {
-                // clone the original model to restore the removed item
-                if ($(e.target).hasClass('layout')) {
-                    vm.layouts = vm.layouts.slice();
-
-                } else if ($(e.target).hasClass('header')) {
-                    vm.headers = vm.headers.slice();
-                } else if ($(e.target).hasClass('item')) {
-                    vm.items = vm.items.slice();
-                }
-            }
+            vm.reset();
         },
         receive: function (e, ui) {
+
         },
         over: function (e, ui) {
         },
         handle: ".fa.fa-arrows",
         placeholder: "placeholder-over",
-        connectWith: ".apps-container"
+        connectWith: ".apps-container",
+        revert: true
     };
-    $scope.getView = function (item) {
+    vm.getView = function (item) {
         /*
          you can return a different url
          to load a different template dynamically
@@ -52,11 +51,7 @@ function PageBuilderController($scope, toastr) {
         }
         return null;
     };
-    vm.test = function (index) {
-        //console.log(index);
-    }
     vm.remove = function (index) {
-        //console.log($scope.items[index]);
         $scope.items.splice(index, 1);
     };
     vm.getName = function () {
@@ -65,4 +60,8 @@ function PageBuilderController($scope, toastr) {
     vm.setName = function (name) {
         vm.name = name;
     };
+
+    vm.reset = function () {
+        vm.components = angular.copy(vm.orig_components);
+    }
 }
