bef53c3eb95c16bd260ff0a7bd1208b4da45d5d3
Mohamed Amine ABBADI <Mohamed.ABBADI@ouicare.net>
2016-12-09 11:36:18 +0100

    Model entity

--- src/AppBundle/Entity/ModelEntity.php
+++ src/AppBundle/Entity/ModelEntity.php
@@ -29,6 +29,20 @@ class ModelEntity {
     private $name;
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
      * @var array
      *
      * @ORM\Column(name="attributes", type="array")
@@ -88,4 +102,52 @@ class ModelEntity {
         return $this->attributes;
     }
 
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return ModelEntity
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return ModelEntity
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 }
