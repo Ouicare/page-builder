d2d767453e0c91e7d7c667d9cc9ad0d91fc9072d
marrouchi <anismarrouchi@hotmail.com>
2016-12-09 15:21:48 +0100

    Fix missing use class in defaultController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\ModelEntity;
+use AppBundle\Form\ModelEntityType;
 
 class DefaultController extends Controller {
 
