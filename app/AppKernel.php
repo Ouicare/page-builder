235d51cba79f6d1625aad6ec378fe6515ef9e346
Mohamed Amine ABBADI <Mohamed.ABBADI@ouicare.net>
2016-12-09 16:21:13 +0100

    Install foSRestBundle + JMSSerilaizer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel {
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
