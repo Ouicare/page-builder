f2813fadc3c3b8b713d8e2551cdf402a4f4bd7ff
Mohamed Amine ABBADI <Mohamed.ABBADI@ouicare.net>
2016-12-14 17:16:33 +0100

    Generate html template

--- src/AppBundle/Controller/ModelController.php
+++ src/AppBundle/Controller/ModelController.php
@@ -3,20 +3,23 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class ModelController extends Controller
-{
-    public function listAction()
-    {
+class ModelController extends Controller {
+
+    public function listAction() {
+        $em = $this->getDoctrine()->getManager();
+        $models = $em->getRepository("AppBundle:Model")->findAll();
         return $this->render('AppBundle:Model:list.html.twig', array(
-            // ...
+                    'models' => $models
         ));
     }
 
-    public function showAction()
-    {
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $currentModel = $em->getRepository("AppBundle:Model")->find($id);
         return $this->render('AppBundle:Model:show.html.twig', array(
-            // ...
+                    'model' => $currentModel
         ));
     }
 
---
+++ web/js/controllers/ModelTemplateController.js
@@ -0,0 +1,24 @@
+app.controller('ModelTemplateController', function ($scope, $http) {
+    $scope.varmodel = "";
+    $scope.models = "";
+    $scope.isInit = false;
+    setTimeout(function () {
+        $http.get(Routing.generate('api_get_entities_models'))
+                .success(function (data, status, headers, config) {
+                    $scope.models = data;
+                    $scope.isInit = true;
+                }).error(function (error, status, headers, config) {
+            $scope.dataLoading = false;
+        });
+    }, 1000);
+
+
+    $scope.selectedModels = function (branch) {
+        $scope.varmodel = "";
+        if (branch.level == 2) {
+            console.log(branch);
+            $scope.varmodel = "{{" + branch.data.parent + "." + branch.name + "}}";
+        }
+        console.log($scope.varmodel);
+    };
+});
